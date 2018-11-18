bc783e5d50dfd641182c365f599d8ce604cbcfd3
wangtengyz <752132694@qq.com>
2018-11-18 19:09:41 +0800

    修改了readme.md和main.js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ import { Header } from 'mint-ui'
 Vue.component(Header.name, Header)
     // 导入mui.css
 import "../libs/mui/css/mui.min.css"
-
-
 //导入App根组件
 import App from './App.vue'
-
 var vm = new Vue({
     el: "#app",
     date: {
